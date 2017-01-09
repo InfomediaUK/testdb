4e9832ba15b61ef9da6ae05e7d0723489ea00b18
Lyndon Smith <lyndon@infomediauk.net>
2017-01-09 14:11:14 +0000

    Uncommented code so that it actually works now...

--- src/main/java/com/helmet/application/CallOffTask.java
+++ src/main/java/com/helmet/application/CallOffTask.java
@@ -6,6 +6,7 @@ import java.util.TimerTask;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 
+import com.helmet.api.MgrService;
 import com.helmet.api.ServiceFactory;
 import com.helmet.bean.BookingGrade;
 
@@ -19,38 +20,38 @@ public class CallOffTask extends TimerTask
 
     logger.debug("***** start *****");
 
-//    MgrService mgrService = ServiceFactory.getInstance().getMgrService();
-//
-//    List<BookingGrade> bookingGrades = mgrService.getBookingGradesToSend();
-//
-//    for (BookingGrade bookingGrade : bookingGrades)
-//    {
-//      // using superuser administrator id - 0
-//      System.out.println("processing booking.bookingGrade - " + bookingGrade.getBookingId() + "." + bookingGrade.getBookingGradeId());
-//      logger.debug("processing booking.bookingGrade - " + bookingGrade.getBookingId() + "." + bookingGrade.getBookingGradeId());
-//      //
-//      // lock the booking whilst opening
-//      try
-//      {
-//        BookingLockHandler.getInstance().addLock(bookingGrade.getBookingId());
-//        int rc = mgrService.updateBookingGradeOpen(bookingGrade.getBookingGradeId(), bookingGrade.getNoOfChanges(), 0);
-//      }
-//      catch (AlreadyLockedRuntimeException e)
-//      {
-//        logger.warn("***** Booking already locked - " + bookingGrade.getBookingGradeId());
-//      }
-//      finally
-//      {
-//        try
-//        {
-//          BookingLockHandler.getInstance().removeLock(bookingGrade.getBookingId());
-//        }
-//        catch (NotLockedRuntimeException e)
-//        {
-//          logger.error("Error trying to remove lock on bookingId - " + bookingGrade.getBookingGradeId());
-//        }
-//      }
-//    }
+    MgrService mgrService = ServiceFactory.getInstance().getMgrService();
+
+    List<BookingGrade> bookingGrades = mgrService.getBookingGradesToSend();
+
+    for (BookingGrade bookingGrade : bookingGrades)
+    {
+      // using superuser administrator id - 0
+      System.out.println("processing booking.bookingGrade - " + bookingGrade.getBookingId() + "." + bookingGrade.getBookingGradeId());
+      logger.debug("processing booking.bookingGrade - " + bookingGrade.getBookingId() + "." + bookingGrade.getBookingGradeId());
+      //
+      // lock the booking whilst opening
+      try
+      {
+        BookingLockHandler.getInstance().addLock(bookingGrade.getBookingId());
+        int rc = mgrService.updateBookingGradeOpen(bookingGrade.getBookingGradeId(), bookingGrade.getNoOfChanges(), 0);
+      }
+      catch (AlreadyLockedRuntimeException e)
+      {
+        logger.warn("***** Booking already locked - " + bookingGrade.getBookingGradeId());
+      }
+      finally
+      {
+        try
+        {
+          BookingLockHandler.getInstance().removeLock(bookingGrade.getBookingId());
+        }
+        catch (NotLockedRuntimeException e)
+        {
+          logger.error("Error trying to remove lock on bookingId - " + bookingGrade.getBookingGradeId());
+        }
+      }
+    }
 
     logger.debug("***** end *****");
 
