21431eaafac7b78f4d3163ad46ae7683fd622fa2
Lyndon Smith <lyndon@infomediauk.net>
2017-05-06 12:51:46 +0100

    New...

---
+++ src/main/java/com/helmet/bean/ApplicantEntity.java
@@ -0,0 +1,91 @@
+package com.helmet.bean;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.List;
+
+public class ApplicantEntity extends Applicant
+{
+  private List<DisciplineCategoryTrainingUser> disciplineCategoryTrainingUsers;
+  private List<ApplicantTrainingCourseUser> applicantTrainingCourseUsers;
+  
+  public List<DisciplineCategoryTrainingUser> getDisciplineCategoryTrainingUsers()
+  {
+    return disciplineCategoryTrainingUsers;
+  }
+
+  public void setDisciplineCategoryTrainingUsers(List<DisciplineCategoryTrainingUser> disciplineCategoryTrainingUsers)
+  {
+    this.disciplineCategoryTrainingUsers = disciplineCategoryTrainingUsers;
+  }
+
+  public List<ApplicantTrainingCourseUser> getApplicantTrainingCourseUsers()
+  {
+    return applicantTrainingCourseUsers;
+  }
+
+  public void setApplicantTrainingCourseUsers(List<ApplicantTrainingCourseUser> applicantTrainingCourseUsers)
+  {
+    this.applicantTrainingCourseUsers = applicantTrainingCourseUsers;
+  }
+
+  public Boolean getHasDisciplineCategoryTrainings()
+  {
+    if (disciplineCategoryTrainingUsers == null)
+    {
+      return false;
+    }
+    return disciplineCategoryTrainingUsers.size() > 0;
+  }
+  
+  public Boolean getHasApplicantTrainingCourses()
+  {
+    if (applicantTrainingCourseUsers == null)
+    {
+      return false;
+    }
+    return applicantTrainingCourseUsers.size() > 0;
+  }
+ 
+  public Boolean hasRequiredTraining(StringBuffer reason)
+  {
+    Calendar calendar = Calendar.getInstance();
+    Date todaysDate = new Date(calendar.getTimeInMillis());
+    Boolean hasRequiredTraining = true;
+    Boolean trainingCourseFound = false;
+    if (getHasDisciplineCategoryTrainings())
+    {
+      for (DisciplineCategoryTrainingUser disciplineCategoryTrainingUser : disciplineCategoryTrainingUsers)
+      {
+        // For each mandatory Training for the Applicant's Discipline Category...
+        trainingCourseFound = false;
+        for (ApplicantTrainingCourseUser applicantTrainingCourseUser : applicantTrainingCourseUsers)
+        {
+          // For each Training Course that the Applicant has taken...
+          if (disciplineCategoryTrainingUser.getTrainingCourseId().equals(applicantTrainingCourseUser.getTrainingCourseId()))
+          {
+            if (todaysDate.after(applicantTrainingCourseUser.getStartDate()) && todaysDate.before(applicantTrainingCourseUser.getEndDate()))
+            {
+              // Applicant has this Training Course current.
+              trainingCourseFound = true;
+              break;
+            }
+          }
+        }
+        if (!trainingCourseFound)
+        {
+          // Applicant has NOT done this mandatory training.
+          reason.append("Current " + disciplineCategoryTrainingUser.getTrainingName() + "\n");
+          hasRequiredTraining = false;
+        }
+      } 
+    }
+    else
+    {
+      reason.append("Discipline Category MISSING Training NOT Checked \n");
+      hasRequiredTraining = false;
+    }
+    return hasRequiredTraining;
+  }
+  
+}
